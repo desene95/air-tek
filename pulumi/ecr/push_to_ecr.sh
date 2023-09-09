5218e987bd49b0367f983695a47b759f2ab7a69a
Damian Esene <damianesene@Damians-MacBook-Air.local>
2023-09-09 12:16:10 -0400

    code to create ecr and push images to ecr

---
+++ pulumi/ecr/push_to_ecr.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+file_name=docker_images.txt
+
+touch $file_name
+
+docker ps >> $file_name
+
+web_image_name=`awk '{ print $2 }' $file_name | tail -2 | head -1`
+api_image_name=`awk '{ print $2 }' $file_name | tail -1`
+
+echo $web_image_name
+echo $api_image_name
+#push web api image
+aws ecr get-login-password --region ca-central-1 | docker login --username AWS --password-stdin 787257093414.dkr.ecr.ca-central-1.amazonaws.com
+
+docker tag $api_image_name:latest 787257093414.dkr.ecr.ca-central-1.amazonaws.com/air_tek_registry-8eb3ca1:$api_image_name
+docker push 787257093414.dkr.ecr.ca-central-1.amazonaws.com/air_tek_registry-8eb3ca1:$api_image_name
+
+
+docker tag $web_image_name:latest 787257093414.dkr.ecr.ca-central-1.amazonaws.com/air_tek_registry-8eb3ca1:$web_image_name
+docker push 787257093414.dkr.ecr.ca-central-1.amazonaws.com/air_tek_registry-8eb3ca1:$web_image_name
+
+
